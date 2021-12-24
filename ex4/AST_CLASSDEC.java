0c93112e08fee0fe033137f63454654e0326dda2
ben.shapira <ben.shapira@algosec.com>
2021-12-24 19:17:38 +0200

    accident

--- ex4/AST_CLASSDEC.java
+++
@@ -1,9 +0,0 @@
-package AST;
-
-import TEMP.TEMP;
-import TYPES.TYPE;
-
-public abstract class AST_CLASSDEC extends AST_Node {
-    public abstract TYPE SemantMe();
-    public abstract TEMP IRme();
-}
